51e3861a7eed2684fcd43291d0f268c146b273c0
Aditya Mazumdar <136506411+aditya95-pixel@users.noreply.github.com>
2025-04-19 15:42:17 +0530

    Update ColorPickerEditor.jsx

--- services/Sharable/ColorPickerEditor.jsx
+++ services/Sharable/ColorPickerEditor.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import {ChromePicker , CirclePicker} from 'react-color'
-
 function ColorPickerEditor({value , onColorChange}) {
   return (
     <div className='space-y-4'>
@@ -9,7 +8,6 @@ function ColorPickerEditor({value , onColorChange}) {
             onChange={(e)=>onColorChange(e.hex)}
             className='border-r rounded-2xl mb-5'
         />
-
         <CirclePicker
             color={value}
             onChange={(e)=>onColorChange(e.hex)}
@@ -17,5 +15,4 @@ function ColorPickerEditor({value , onColorChange}) {
     </div>
   )
 }
-
 export default ColorPickerEditor
