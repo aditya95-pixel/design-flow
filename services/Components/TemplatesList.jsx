248382481b59669421fa7ad8839b1705cdd94cc7
Soumyadip Maity <129609774+soumyadip23m@users.noreply.github.com>
2025-04-19 15:40:45 +0530

    Update TemplatesList.jsx

--- services/Components/TemplatesList.jsx
+++ services/Components/TemplatesList.jsx
@@ -9,7 +9,6 @@ function TemplatesList() {
     const onTemplateSelect=(template)=>{
         if(!canvasEditor) return;
         setUndoStack(prev => [...prev, canvasEditor.toJSON()]);
-        // Clear redo stack when a new action is performed
         setRedoStack([]);
         if(canvasEditor){
             canvasEditor.loadFromJSON(template?.jsonData,()=>{
