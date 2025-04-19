5ba52721a5b16ce94ab2cc725705381f4f259362
Soumyadip Maity <129609774+soumyadip23m@users.noreply.github.com>
2025-04-19 17:53:09 +0530

    Update SideBarSettings.jsx

--- app/(routes)/design/_components/SideBarSettings.jsx
+++ app/(routes)/design/_components/SideBarSettings.jsx
@@ -4,10 +4,9 @@ import { Button } from '../../../../components/ui/button'
 
 function SideBarSettings({ selectedOption }) {
   return (
-    <div className='text-red-600 font-sans w-[280px] p-5 h-screen border-r dark:bg-gray-900 bg-rose-50'>
+    <div className='dark:text-cyan-200 text-red-800 font-sans w-[280px] p-5 h-screen border-r dark:bg-gray-900 bg-rose-50'>
       <h2 className='font-bold'>{selectedOption?.name}</h2>
-      
-      <p className='text-sm text-red-700 font-bold'>{selectedOption?.desc}</p>
+      <p className='text-sm dark:text-cyan-200 text-red-800 font-bold'>{selectedOption?.desc}</p>
 
       <div className='mt-7'>
         {selectedOption?.link ? (
@@ -27,4 +26,5 @@ function SideBarSettings({ selectedOption }) {
     </div>
   )
 }
+
 export default SideBarSettings
