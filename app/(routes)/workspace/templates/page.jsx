77e50c608964399185f801b6ab572f7c5c2811d9
Soumyadip Maity <129609774+soumyadip23m@users.noreply.github.com>
2025-04-20 02:25:24 +0530

    Update page.jsx

--- app/(routes)/workspace/templates/page.jsx
+++ app/(routes)/workspace/templates/page.jsx
@@ -7,9 +7,21 @@ function Templates() {
   return (
     <div className="p-10 w-full dark:bg-gray-900 bg-rose-100 min-h-screen">
         <div className='relative'>
-            <Image src={"/banner.png"} alt="banner" width={1800} height={300}
-                        className='w-full h-[200px] rounded-2xl object-cover mt-12'/>
-                        <h2 className='text-3xl absolute bottom-5 left-10 dark:text-white text-black'>Templates</h2>
+           <Image 
+                     src={"/banner-home.png"}
+                     alt="banner" 
+                     width={1800} 
+                     height={300}
+                     className='w-full h-[150px] sm:h-[180px] md:h-[200px] rounded-xl dark: md:rounded-2xl object-cover mt-10 dark:hidden'
+                   />
+                   <Image 
+                     src={"/banner.png"}
+                     alt="banner" 
+                     width={1800} 
+                     height={300}
+                     className='w-full h-[150px] sm:h-[180px] md:h-[200px] rounded-xl dark: md:rounded-2xl object-cover mt-10 hidden dark:block'
+                     />
+                     <h2 className='text-3xl absolute bottom-5 left-10 dark:text-white text-cyan-200'>Templates</h2>
         </div>
         <PreTemplatesList/>
     </div>
